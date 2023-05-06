26dea09d3c035b418f7672dd72ab478e0fb499a7
Evg <konstantinoveo@gmail.com>
2023-05-06 01:25:46 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -2,7 +2,7 @@ module "network" {
   source  = "app.terraform.io/hinjc/network/google"
   version = "3.4.0"
     project_id   = var.project
-    network_name = "gaurav-network"
+    network_name = "gaurav-network2"
     routing_mode = "GLOBAL"
 
     subnets = [
